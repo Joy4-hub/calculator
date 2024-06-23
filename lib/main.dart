5b6d5239e0917f84ff8e00eb418941437a9be07c
Joy4-hub <158175637+Joy4-hub@users.noreply.github.com>
2024-06-23 08:43:15 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HelloScreen(),
+        '/calculator': (context) => CalculatorScreen(),
+      },
+    );
+  }
+}
+
+class HelloScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello, World!'
+              'WELCOME TO OUR SIMPLE CALCULATOR ',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/calculator');
+              },
+              child: Text('Go to Calculator'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _output = "0";
+  String _currentInput = "";
+  String? _operation;
+  double _num1 = 0;
+  double _num2 = 0;
+
+  void _buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _output = "0";
+      _currentInput = "";
+      _operation = null;
+      _num1 = 0;
+      _num2 = 0;
+    } else if (buttonText == "+" || buttonText == "-" || buttonText == "/" || buttonText == "*") {
+      _num1 = double.parse(_currentInput);
+      _operation = buttonText;
+      _currentInput = "";
+    } else if (buttonText == ".") {
+      if (_currentInput.contains(".")) {
+        return;
+      } else {
+        _currentInput = _currentInput + buttonText;
+      }
+    } else if (buttonText == "=") {
+      _num2 = double.parse(_currentInput);
+
+      if (_operation == "+") {
+        _output = (_num1 + _num2).toString();
+      } else if (_operation == "-") {
+        _output = (_num1 - _num2).toString();
+      } else if (_operation == "*") {
+        _output = (_num1 * _num2).toString();
+      } else if (_operation == "/") {
+        _output = (_num1 / _num2).toString();
+      }
+
+      _num1 = 0;
+      _num2 = 0;
+      _operation = null;
+      _currentInput = _output;
+    } else {
+      _currentInput = _currentInput + buttonText;
+      _output = _currentInput;
+    }
+
+    setState(() {});
+  }
+
+  Widget _buildButton(String buttonText) {
+    return Expanded(
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: Color.fromARGB(255, 13, 13, 13), padding: EdgeInsets.all(10.0),
+          backgroundColor: Color.fromARGB(139, 126, 7, 7),
+        ),
+        onPressed: () => _buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(
+              vertical: 24.0,
+              horizontal: 12.0,
+            ),
+            child: Text(
+              _output,
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  _buildButton("7"),
+                  _buildButton("8"),
+                  _buildButton("9"),
+                  _buildButton("/"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("4"),
+                  _buildButton("5"),
+                  _buildButton("6"),
+                  _buildButton("*"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("1"),
+                  _buildButton("2"),
+                  _buildButton("3"),
+                  _buildButton("-"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("."),
+                  _buildButton("0"),
+                  _buildButton("00"),
+                  _buildButton("+"),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  _buildButton("C"),
+                  _buildButton("="),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
